5bc0be5995cb77de1cd66adfe7b414667e2e1177
PUJA <puja.ec676@gmail.com>
2021-01-19 11:47:39 +0530

    removed taxx

--- small-shopping-cart/src/components/Basket.js
+++ small-shopping-cart/src/components/Basket.js
@@ -4,8 +4,8 @@ export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
   const taxPrice = itemsPrice * 0.14;
-  const shippingPrice = itemsPrice > 2000 ? 0 : 20;
-  const totalPrice = itemsPrice + taxPrice + shippingPrice;
+  //const shippingPrice = itemsPrice > 2000 ? 0 : 20;
+  const totalPrice = itemsPrice ;
   return (
     <aside className="block col-1">
       <h2>Cart Items</h2>
@@ -36,16 +36,16 @@ export default function Basket(props) {
               <div className="col-2">Items Price</div>
               <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
             </div>
-            <div className="row">
+            {/* <div className="row">
               <div className="col-2">Tax Price</div>
               <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
+            </div> */}
+            {/* <div className="row">
               <div className="col-2">Shipping Price</div>
               <div className="col-1 text-right">
                 ${shippingPrice.toFixed(2)}
               </div>
-            </div>
+            </div> */}
 
             <div className="row">
               <div className="col-2">
